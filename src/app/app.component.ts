04a1446562785d15035b2f291ef7115ded0a5660
AlejandroHV <alejandro.herreno@hotmail.com>
2023-06-26 23:26:18 -0500

    Adding a TODO Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  
-
+ /* TO DO: 
+    • Improve styling. Implement the basic theme from Material. 
+    • Improve user experience.
+    • When calculating the due date by the span Weekly and BiWeekly the dates might fall outside the month.
+    • The calculation is currently being done in a state machine way but it can be implemented as a recursive 
+      function for the weekly and holidays part and also going back to the start of the process.
+    • Improve the ussage of the Form Control. Dynamic fields, Validations, messages. 
+    
+ */
 }
